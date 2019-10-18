fbe0f9619dbb5370db228cfed7a26279f054b458
sushen <sushenbiswasaga@gmail.com>
2019-10-18 16:01:35 +0600

    Add API Clint

---
+++ app/src/main/java/blog/video/biswas/retrofit/model/ApiClint.java
@@ -0,0 +1,18 @@
+package blog.video.biswas.retrofit.model;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClint {
+    static Retrofit getClint() {
+
+        OkHttpClient client = new OkHttpClient.Builder().build();
+        return new Retrofit.Builder()
+                .baseUrl("http://nurhossen.info/appsHill/api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+
+    }
+}
